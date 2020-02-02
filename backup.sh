9a41c18ef78aee4ca94d14adf9b26468e913ff44
Taman <twinp.st@gmail.com>
2020-02-02 02:43:33 +0000

    Update...

--- backup.sh
+++ backup.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
+cd ~/git-projects/vscode-settings/
 ls ~/.vscode/extensions/ > extensions.txt
 cp ~/Library/Application\ Support/Code/User/settings.json settings.json
 
 git commit -am "Update..."
 git push
+
+echo "backup completed!"
